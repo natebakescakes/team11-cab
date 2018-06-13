76be4eea437c7284b5db1a1ab103be7a815fb893
weinchester <weinchester@gmail.com>
2018-06-13 22:02:34 +0800

    logical conflicts before pull request

--- src/main/java/com/team11/cab/JsonController.java
+++ src/main/java/com/team11/cab/JsonController.java
@@ -4,9 +4,6 @@ import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-
-import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
--- src/main/java/com/team11/cab/MemberController.java
+++ src/main/java/com/team11/cab/MemberController.java
@@ -3,9 +3,7 @@ package com.team11.cab;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestBody;
